1cdc87711fc432603e0930f5ada032964d693090
reginmund9 <moronitaca@gmail.com>
2024-05-04 23:24:54 -0600

    w2-DynamicPL

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import { ProductData } from "./ProductData.mjs";
+const dataSource = new ProductData("tents");
+
+import ProductList from "./ProductList.mjs"; 
+// Assuming you have an element with id 'product-list' in your HTML
+//const listElement = document.getElementById('product-list'); 
+const element = document.querySelector(".product-list");
+const listing = new ProductList("Tents", dataSource, element);
+listing.init();
+
