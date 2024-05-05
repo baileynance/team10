fc25d7ef1437895b5de8fa32b9ee687ab0dd07b5
reginmund9 <moronitaca@gmail.com>
2024-05-04 23:28:02 -0600

    w2-dPL

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
 import { ProductData } from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
-import ProductList from "./ProductList.mjs"; 
+import ProductList from "./ProductList.mjs";
 // Assuming you have an element with id 'product-list' in your HTML
-//const listElement = document.getElementById('product-list'); 
+//const listElement = document.getElementById('product-list');
 const element = document.querySelector(".product-list");
 const listing = new ProductList("Tents", dataSource, element);
 listing.init();
-
