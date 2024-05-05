d21597192937a69db6bdd4aea3e5611f3810b174
baileynance <baieyj.nance@gmail.com>
2024-05-05 10:47:02 -0700

    individual activity

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -2,13 +2,15 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
     return `<li class="product-card">
-      <a href="product_pages/index.html?product=">
-        <img src="" alt="Image of ">
-        <h3 class="card__brand"></h3>
-        <h2 class="card__name"></h2>
-        <p class="product-card__price">$</p>
-      </a>
-    </li>`
+    <a href="product_pages/index.html?product=${product.Id}">
+    <img
+      src="${product.Image}"
+      alt="Image of ${product.Name}"
+    />
+    <h3 class="card__brand">${product.Brand.Name}</h3>
+    <h2 class="card__name">${product.Name}</h2>
+    <p class="product-card__price">$${product.FinalPrice}</p></a>
+    </li>`;
 }
 
 export default class ProductListing {
@@ -24,6 +26,6 @@ export default class ProductListing {
     }
 
     renderList(list) {
-        renderListWithTemplate(productCardTemplate, this.listElement, list);
+        renderListWithTemplate(productCardTemplate, this.listElement, list.slice(0, 4));
     }
 };
